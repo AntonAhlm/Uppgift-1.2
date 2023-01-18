107cf18d4e01714d7ccd67b469a51986c723e931
Acrobaticbag3 <kevintom214@gmail.com>
2023-01-18 10:04:55 +0100

    Add project files.

---
+++ Uppgift 1.2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Windows
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Clear();
+            Console.WriteLine("An error has occured. To continue: \n ");
+            Console.WriteLine("Press Enter to return to Windows \n");        
+
+
+        }
+    }
+}
+
+
+
